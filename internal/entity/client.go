cde8de4d2a2cb13ef48f907ba7bccafdf9867596
Avraham Ben Aram <avrahambenaramsouza@gmail.com>
2024-11-07 22:52:08 -0300

    chore: set cpf size to max of 11

--- internal/entity/client.go
+++ internal/entity/client.go
@@ -5,5 +5,5 @@ type Client struct {
 	Name  string `json:"name" gorm:"not null"`
 	Email string `json:"email" gorm:"not null;unique"`
 	Phone string `json:"phone" gorm:"not null"`
-	CPF   string `json:"cpf" gorm:"not null;unique"`
+	CPF   string `json:"cpf" gorm:"not null;unique;size:11"`
 }
