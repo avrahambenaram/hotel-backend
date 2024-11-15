a7ae83fcd7d96ccc500613d933128cf2258b9fa7
Avraham Ben Aram <avrahambenaramsouza@gmail.com>
2024-11-15 14:39:38 -0300

    fix(reservation): receive roomid and clientid in body

--- internal/entity/reservation.go
+++ internal/entity/reservation.go
@@ -6,8 +6,8 @@ type Reservation struct {
 	ID       uint      `json:"id" gorm:"primaryKey"`
 	CheckIn  time.Time `json:"checkIn" gorm:"not null"`
 	CheckOut time.Time `json:"checkOut" gorm:"not null"`
-	RoomID   uint
-	ClientID uint
+	RoomID   uint      `json:"roomId"`
+	ClientID uint      `json:"clientId"`
 
 	Room   HotelRoom `json:"room"`
 	Client Client    `json:"client"`
