2c9941c7b76fd13e3819f8767d1001fd724d8435
Avraham Ben Aram <avrahambenaramsouza@gmail.com>
2024-11-22 14:57:52 -0300

    chore(reservation): cascade onUpdate and onDelete

--- internal/entity/reservation.go
+++ internal/entity/reservation.go
@@ -6,9 +6,9 @@ type Reservation struct {
 	ID       uint      `json:"id" gorm:"primaryKey"`
 	CheckIn  time.Time `json:"checkIn" gorm:"not null"`
 	CheckOut time.Time `json:"checkOut" gorm:"not null"`
-	RoomID   uint      `json:"roomId"`
-	ClientID uint      `json:"clientId"`
+	RoomID   uint      `json:"roomId" gorm:"not null"`
+	ClientID uint      `json:"clientId" gorm:"not null"`
 
-	Room   *HotelRoom `json:"room"`
-	Client *Client    `json:"client"`
+	Room   *HotelRoom `json:"room" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	Client *Client    `json:"client" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 }
